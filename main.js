34d8b6f12c8d305981ae2311e079669eeb5d4813
thomaslwq <thomaslwq@gmail.com>
2013-10-09 17:45:07 +0800

    add multiselect feature

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
 	
-	$('#edit_input').ieditable('http://localhost/save',{
-		indicator:'saving',
-		tooltip:'click to edit',
-		onblur:'submit'
-	});
+	  $('#edit_input').ieditable('testdata/saveurl.json', {
+            type: 'multiselect',
+            multiple:false,
+            indicator: '正在保存...',
+            onblur: 'submit',
+            ajaxoptions: {
+            	dataType: 'json'
+            },
+            init:{
+               init_url:'testdata/multiselect.json',
+               //返回结果的json数据
+               init_callback:function(data,system_callback){
+               	 //处理后台传来的数据，转成[{id:123,text:123}]的形式
+               	if(data['success'])
+               	{
+					
+               		
+	               	var returnData = {id:"text",text:"text"}
+
+	               	system_callback(returnData);	               		
+               	}else{
+               		alert('网络连接超时');
+               	}
+               },
+			   
+               init_params:{
+				search_url:'testdata/multiselect.json'
+//                   id:id
+               }
+            },           
+            submitdata: {
+				// id:id
+            },
+            style: 'display: inline;width:50px',
+            callback: function(data, settings){
+            	if(data['success'])
+            	{
+                $(this).html("修改成功");
+            	}else{
+            		alert('网络连接超时：'+data.msg);
+            	}
+            }
+       })
 	
 });
---
+++ plugins/jquery.ieditable.select.js
@@ -0,0 +1,172 @@
+/*
+ * 
+ * 自定义jeditable的可视化编辑器，依赖bootrap的可视化编辑器
+ * http://www.bootcss.com/p/bootstrap-wysiwyg/
+ */
+ $.ieditable.addInputType('multiselect',{
+    element : function(settings, original) {
+        var div = $('<input style="width:50%"/>');
+        if (settings.width  != 'none') { div.attr('width', settings.width);  }
+        if (settings.height != 'none') { div.attr('height', settings.height); }
+        /* https://bugzilla.mozilla.org/show_bug.cgi?id=236791 */
+        //input[0].setAttribute('autocomplete','off');
+        $(this).append(div);
+        return(div);
+    },
+    content : function(string, settings, original) {
+        $(this).find("input:first").val(string);
+    },
+    //添加第三方插件wysiwyg的 支持
+    plugin:function(settings,original){
+    	var self = original;
+    	var select_input = $(original).find('input:first');
+    	var search_url = '';
+			select_input.select2({
+						minimumInputLength: 2,			
+						multiple: settings.multiple||false,
+					    initSelection : function (element, callback) {
+						if(settings.init){
+							var init = settings.init;
+							var init_data = init.init_data||'';
+							var init_url = init.init_url||'';
+							var init_callback = init.init_callback||function(){};
+							var init_params = init.init_params||'';
+							if(init_params){
+								search_url = init_params.search_url;
+							}
+							
+							if(init_data){
+								select_input.select2('val',init_data);
+							}else{
+						    	 $.ajax({
+						            type:'get',
+						            url: init_url,
+						            data: init_params,
+						            dataType: 'json',
+						            success: function(data) {
+										init_callback(data,callback)
+									}
+						        });				
+							}
+						}else{					    	
+					        var data = [];
+					        $(element.val().split(",")).each(function () {
+					            data.push({id: this, text: this});
+					        });
+					        callback(data);
+						}
+						select_input.on('select2-blur',function(e){
+				                              var submitdata = {};
+				                              if(settings.multiple)
+				                              {
+						                           var   input_values= select_input.select2('val');                                   
+						                            submitdata[settings.name] = input_values.join(",");
+				                              }else{
+					                              submitdata[settings.name] = select_input.select2('val');                              
+				                              }
+				
+				                              submitdata[settings.id] = self.id;
+				                              /* Add extra data to be POST:ed. */
+				                              if ($.isFunction(settings.submitdata)) {
+				                                  $.extend(submitdata, settings.submitdata.apply(self, [self.revert, settings]));
+				                              } else {
+				                                  $.extend(submitdata, settings.submitdata);
+				                              }
+				
+				                              /* Quick and dirty PUT support. */
+				                              if ('PUT' == settings.method) {
+				                                  submitdata['_method'] = 'put';
+				                              }
+				
+				                              /* Show the saving indicator. */
+				                              $(self).html(settings.indicator);
+				                              
+				                              /* Defaults for ajaxoptions. */
+				                              var callback = settings.callback||function(){};
+				                              var ajaxoptions = {
+				                                  type    : 'POST',
+				                                  data    : submitdata,
+				                                  dataType: 'html',
+				                                  url     : settings.target,
+				                                  success : function(result, status) {
+				                                      if (ajaxoptions.dataType == 'html') {
+				                                        $(self).html(result);
+				                                      }
+				                                      self.editing = false;
+				                                      callback.apply(self, [result, settings]);
+				                                      if (!$.trim($(self).html())) {
+				                                          $(self).html(settings.placeholder);
+				                                      }
+				                                  },
+				                                  error   : function(xhr, status, error) {
+													if(settings.onerror){
+													
+														settings.onerror.apply(original, [settings, self, xhr]);
+													
+													}else{
+														alert('responseText:'+xhr.responseText+' status:'+status);
+													}
+				                                      
+				                                  }
+				                              };
+				                              
+				                              /* Override with what is given in settings.ajaxoptions. */
+				                              $.extend(ajaxoptions, settings.ajaxoptions);   
+				                              $.ajax(ajaxoptions); 			
+						
+							
+						});						
+					    },					
+			            query: function (query) {
+							    	 $.ajax({
+							            type:'get',
+							            url: search_url,
+							            data:  {
+												'query_name' : query.term
+											},
+							            dataType: 'json',
+							            success: function(data) {
+												var data = data.data;
+												var queryData = {results: []}
+												var len = data.length;
+												var options = '';
+												for (var i = 0; i < len; i++) {
+													queryData.results.push({id:data[i]['id'] , text: data[i]['text']});
+												}
+												query.callback(queryData);
+											},
+										error   : function(xhr, status, error) {
+													if(settings.onerror){
+													
+														settings.onerror.apply(original, [settings, self, xhr]);
+													
+													}else{
+														alert('responseText:'+xhr.responseText+' status:'+status);
+													}
+				                                      
+				                                  }
+							        });			            	
+			            }		 
+			});
+//填充编辑时的初始化数据
+			
+
+			
+//		var members_data = {
+//			members_cn_names:$(original).attr('members_cn_names'),
+//			members_en_names:$(original).attr('members_en_names'),			
+//		};
+//		select_input.select2('val',members_data);
+		
+//		select_input.bind('select2-close',function(e){
+//			$(original).html(self.revert);
+//			
+//		});
+
+    	
+    },
+    /*阻止jieditor自己默认的提交*/
+    onsubmit:function(){
+    	return false;
+    }
+ })
